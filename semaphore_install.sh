5c420cfdfad48f2e1608adb67cde0857f0c233ca
Giacomo Zarba Meli <giacomo.zarba@immobiliare.it>
2023-10-08 16:47:20 +0200

    fix ansible opentelemetry

--- semaphore_install.sh
+++ semaphore_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt-get install snapd -ysnap install semaphore
+apt-get install snapd -y
 snap install semaphore
-semaphore user add --admin --login gzarba --name=GiacomoZarbaMeli --email=gzarbameli@gmail.com --password=12345
+/snap/bin/semaphore user add --admin --login gzarba --name=GiacomoZarbaMeli --email=gzarbameli@gmail.com --password=12345
 snap set semaphore port=3001 interface=192.168.1.10
 snap start semaphore
 
